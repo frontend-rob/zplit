1a16b1c924c73e646643e47d6775dd007286272d
R.G. <frontend-rob@proton.me>
2025-08-18 16:35:20 +0200

    chore(firebase): add firebase config and setup environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+/**
+ * This file is for reference only in development environments.
+ * The actual Firebase keys are maintained exclusively in environment.ts.
+ * Do not enter any real or production keys here!
+ */
+export const environment = {
+    firebase: {
+        apiKey: "YOUR API KEY HERE",
+        authDomain: "YOUR AUTH DOMAIN HERE",
+        projectId: "YOUR PROJECT ID HERE",
+        storageBucket: "YOUR STORAGE BUCKET HERE",
+        messagingSenderId: "YOUR MESSAGING SENDER ID HERE",
+        appId: "YOUR APP ID HERE"
+    }
+};
