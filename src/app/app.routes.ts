75478a5fe717aea4d1a85fb568a084f2c132493b
R.G. <frontend-rob@proton.me>
2025-08-19 19:04:21 +0200

    chore: initial project structure with pages, features, components and stylesheets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { WorkspaceComponent } from './pages/workspace/workspace.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent,
+        title: 'Zplit',
+    },
+    {
+        path: 'workspace',
+        component: WorkspaceComponent,
+        title: 'Zplit - Workspace',
+        canActivate: [authGuard]
+    },
+    {
+        path: 'legal-notice',
+        component: LegalNoticeComponent,
+        title: 'Zplit - Legal Notice',
+    },
+    {
+        path: 'privacy-policy',
+        component: PrivacyPolicyComponent,
+        title: 'Zplit - Privacy Policy',
+    }
+];
